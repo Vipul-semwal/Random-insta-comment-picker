0ce60b11b06f39a8400bbf455a3971d4a3cdd74b
ashutosh yadav <ashutoshyadav81787@gmail.com>
2025-06-27 14:01:35 +0000

    port bs

--- index.js
+++ index.js
@@ -183,4 +183,5 @@ function extractMediaCode(url) {
   return match ? match[1] : null;
 }
 
-app.listen(process.env.PORT, () => console.log(`Server running on http://localhost:${process.env.PORT}`));
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
